5b5acbcd793b2cd68b0734e129c3829ecb5b52dc
Iliana Bobeva <iliana.bobeva@gmail.com>
2015-08-22 18:57:47 +0300

    Add NUnit test project

---
+++ tests/HagmanGameTests/PlayerTest.cs
@@ -0,0 +1,31 @@
+﻿using HangmanGame.HangmanGame;
+using NUnit.Framework;
+
+namespace HagmanGameTests
+{
+    [TestFixture]
+    public class PlayerTest
+    {
+        private Player player;
+
+        [SetUp]
+        public void Init()
+        {
+            player = new Player();
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            player = null;
+        }
+
+        [Test]
+        public void TestIncreaseMistakes()
+        {
+            player.IncreaseMistakes();
+
+            Assert.AreEqual(1, player.Mistakes, "Player mistakes should be 1 when increased once after initialization");
+        }
+    }
+}
