a44158df5fde15ef29c6b838f84fc8557d9e8b86
deyantodorov <deyan.todorov@hotmail.com>
2015-10-04 22:41:26 +0300

    GuessWordTest.cs test added

---
+++ tests/HagmanGameTests/GuessWordTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using HangmanGame.HangmanGame.Common;
+using NUnit.Framework;
+
+namespace HagmanGameTests
+{
+    [TestFixture]
+    public class GuessWordTest
+    {
+        private const string Word = "highqualitycode";
+        private const string WordMasked = "_______________";
+        private GuessWord guessWord;
+
+        [SetUp]
+        public void Init()
+        {
+            this.guessWord = new GuessWord(Word);
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            this.guessWord = null;
+        }
+
+        [Test]
+        public void TestContentValue()
+        {
+            Assert.AreEqual(Word, this.guessWord.Content, "Content of GuessWord should be equal");
+        }
+
+        [Test]
+        public void TestMaskingOfWord()
+        {
+            Assert.AreEqual(WordMasked, string.Join("", this.guessWord.Mask), "Masked word should be equal to GuessWord.Mask");
+        }
+
+        [TestCase(1, 'y')]
+        [TestCase(0, 'z')]
+        [TestCase(2, 'h')]
+        [TestCase(1, 'e')]
+        public void TestGetNumberOfOccurences(int found, char letter)
+        {
+            Assert.AreEqual(found, this.guessWord.GetNumberOfOccurences(letter), "Letter '" + letter + "' should be found only once");
+        }
+    }
+}
